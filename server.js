d7d3807e4dbed17b84e10af4508e536560685414
Gonzalez, Olivia <olivia.gonzalez@elekta.com>
2015-04-29 10:32:40 -0500

    Session 2: Last steps

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 'use strict';
 var Hapi = require('hapi');
+var GoodWinston = require('good-winston');
+var winston = require('winston');
 var server = new Hapi.Server();
+server.register({
+register: require('good'),
+  options: {
+    reporters: [
+      new GoodWinston({
+        ops: '*',
+        request: '*',
+        response: '*',
+        log: '*',
+        error: '*'
+      }, winston)
+    ]
+  }
+}, function(err) {
+  if(err) {
+    return server.log(['error'],'good load error: ' + err);
+  }
+});
 
 if( process.env.PORT ) {
   server.connection({ port: process.env.PORT });
