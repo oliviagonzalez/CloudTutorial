71339a6e23d72651025f4cc8b5fa759238597d72
Gonzalez, Olivia <olivia.gonzalez@elekta.com>
2015-05-06 15:15:37 -0500

    removed old server.route from server.js

--- server.js
+++ server.js
@@ -41,28 +41,13 @@ server.route({
 	}
 
 });
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
 
 server.route({
   method : "GET",
   path : '/{param*}',
   handler : {
     directory : {
-      path : '/public',
+      path : 'public',
   listing : true
     }
   }
